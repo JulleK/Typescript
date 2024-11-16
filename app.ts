73f8b9a7e8611868699571d53b438a68ebb9f452
Julian <97186369+JulleK@users.noreply.github.com>
2024-11-16 21:18:37 +0100

    function, union types etc.

--- app.ts
+++ app.ts
@@ -1,19 +1,12 @@
-function combine(input1: number | string, input2: number | string) {
-  let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "julek";
+// userName = userInput;
+
+function generateError(message: string, code: number) {
+  throw { message, errorCode: code };
 }
 
-type YesOrNo = "yes" | "no";
-
-const n1 = 5;
-const n2 = 2.7;
-
-const result = combine(n1, n2);
-console.log(result);
-
-let isTypescriptFun: YesOrNo;
+const result = generateError("An error occurred!", 500);
