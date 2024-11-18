5a3e0763afdef9c3553ba45de4f35acf871673fa
Julian <97186369+JulleK@users.noreply.github.com>
2024-11-18 18:31:08 +0100

    classes in typescript

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,28 @@
 "use strict";
-const button = document.querySelector("button");
-if (button) {
-    button.addEventListener("click", () => {
-        let name = "max";
-        console.log("hello, " + name);
-    });
+class Person {
+    constructor(name) {
+        this.name = name;
+        // name: string;
+        this.hobbies = [];
+    }
+    greet() {
+        console.log("hello " + this.name);
+    }
+    addHobby(hobby) {
+        this.hobbies.push(hobby);
+    }
+    printHobbies() {
+        console.log(this.hobbies);
+    }
+    printName() {
+        console.log(this.name);
+    }
 }
+const max = new Person("max");
+max.greet();
+max.addHobby("programming");
+max.hobbies[1] = "hacking";
+max.printHobbies();
+// const maxCopy = { greet: max.greet };
+// maxCopy.greet();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,34 @@
-const button = document.querySelector("button");
+class Person {
+  // name: string;
+  hobbies: string[] = [];
 
-if (button) {
-  button.addEventListener("click", () => {
-    let name = "max";
-    console.log("hello, " + name);
-  });
+  constructor(private name: string) {}
+
+  greet(this: Person) {
+    console.log("hello " + this.name);
+  }
+
+  addHobby(hobby: string) {
+    this.hobbies.push(hobby);
+  }
+
+  printHobbies() {
+    console.log(this.hobbies);
+  }
+
+  private printName() {
+    console.log(this.name);
+  }
 }
+
+const max = new Person("max");
+
+max.greet();
+max.addHobby("programming");
+
+max.hobbies[1] = "hacking";
+max.printHobbies();
+
+// const maxCopy = { greet: max.greet };
+
+// maxCopy.greet();
