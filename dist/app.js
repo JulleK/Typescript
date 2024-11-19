a6d1bd5a8ab6bfdadd95c1c3c5f01e7de5458606
Julian <97186369+JulleK@users.noreply.github.com>
2024-11-19 18:51:28 +0100

    inheritance 😳

--- dist/app.js
+++ dist/app.js
@@ -1,28 +1,52 @@
 "use strict";
-class Person {
-    constructor(name) {
+class Department {
+    showYear() {
+        console.log(Department.creationDate);
+    }
+    constructor(id, name) {
+        this.id = id;
         this.name = name;
-        // name: string;
-        this.hobbies = [];
+        this.employees = [];
     }
-    greet() {
-        console.log("hello " + this.name);
+    addEmployee(employee) {
+        this.employees.push(employee);
     }
-    addHobby(hobby) {
-        this.hobbies.push(hobby);
+    get lastEmployee() {
+        if (this.employees) {
+            return this.employees[this.employees.length - 1];
+        }
+        throw new Error("No employee found");
     }
-    printHobbies() {
-        console.log(this.hobbies);
+    set lastEmployee(value) {
+        this.addEmployee(value);
     }
-    printName() {
-        console.log(this.name);
+    static createEmployee(name, age, section) {
+        return { name, age, section };
     }
 }
-const max = new Person("max");
-max.greet();
-max.addHobby("programming");
-max.hobbies[1] = "hacking";
-max.printHobbies();
-// const maxCopy = { greet: max.greet };
-// maxCopy.greet();
+Department.creationDate = 2024;
+class ITDepartment extends Department {
+    constructor(id) {
+        super(id, "IT");
+    }
+    printEmployees() {
+        console.log(this.employees);
+    }
+    describe() {
+        console.log("IT Department - ID:" + this.id);
+    }
+    static getInstance() {
+        if (!this.instance) {
+            this.instance = new ITDepartment("1");
+        }
+        return this.instance;
+    }
+}
+const d = new Department("15", "itboy");
+d.addEmployee("hej");
+d.addEmployee("ja");
+d.lastEmployee = "me";
+console.log(d.lastEmployee);
+const emp1 = Department.createEmployee("julek", 18, "it");
+const IT = ITDepartment.getInstance();
 //# sourceMappingURL=app.js.map
