e2dffa48842195b35b5ae3f97de72b2437040794
Julian <97186369+JulleK@users.noreply.github.com>
2024-11-19 19:37:37 +0100

    interfaces basics

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,14 @@
 "use strict";
+// interface Person {
+//   name: string;
+//   age: number;
+class Person {
+    constructor(name) {
+        this.age = 30;
+        this.name = name;
+    }
+    greet(phrase) {
+        console.log(phrase + " " + this.name);
+    }
+}
 //# sourceMappingURL=app.js.map
