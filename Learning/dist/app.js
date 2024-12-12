825f31ee1e173ef6652a61d5065cced14c7ccc55
Julian <97186369+JulleK@users.noreply.github.com>
2024-12-12 21:02:22 +0100

    more decorators :o

--- Learning/dist/app.js
+++ Learning/dist/app.js
@@ -5,19 +5,59 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logger(target) {
-    console.log("Logging...");
-    console.log(target);
+function Logger(logString) {
+    return function (constructor) {
+        console.log(logString);
+        console.log(constructor);
+    };
 }
-let Person = class Person {
+function WithTemplate(hookId) {
+    return function (constructor) {
+        const p = new constructor();
+        const element = document.getElementById(hookId);
+        if (element) {
+            element.innerHTML = p.name;
+        }
+    };
+}
+function FirstDecorator() {
+    console.log("I am first");
+    return function (constructor) { };
+}
+function SecondDecorator() {
+    console.log("I am second");
+    return function (constructor) { };
+}
+// @SecondDecorator()
+// @FirstDecorator()
+// @WithTemplate("app")
+class Person {
     constructor() {
         this.name = "julek";
         console.log("Creating a person...");
     }
-};
-Person = __decorate([
-    Logger
-], Person);
-const pers = new Person();
+}
+// const pers = new Person();
+function Log(target, propertyName) {
+    console.log("Property decorator");
+    console.log(target);
+    console.log(propertyName);
+}
+class Product {
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    set price(value) {
+        if (value > 0)
+            this._price = value;
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
 // console.log(pers);
 //# sourceMappingURL=app.js.map
--- Learning/src/app.ts
+++ Learning/src/app.ts
@@ -1,11 +1,33 @@
-function Logger() {
+function Logger(logString: string) {
   return function (constructor: Function) {
-    console.log("Logging...");
+    console.log(logString);
     console.log(constructor);
   };
 }
 
-@Logger()
+function WithTemplate(hookId: string) {
+  return function (constructor: any) {
+    const p = new constructor();
+    const element = document.getElementById(hookId);
+    if (element) {
+      element.innerHTML = p.name;
+    }
+  };
+}
+
+function FirstDecorator() {
+  console.log("I am first");
+  return function (constructor: Function) {};
+}
+
+function SecondDecorator() {
+  console.log("I am second");
+  return function (constructor: Function) {};
+}
+
+// @SecondDecorator()
+// @FirstDecorator()
+// @WithTemplate("app")
 class Person {
   name = "julek";
 
@@ -14,6 +36,31 @@ class Person {
   }
 }
 
-const pers = new Person();
+// const pers = new Person();
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator");
+  console.log(target);
+  console.log(propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  set price(value: number) {
+    if (value > 0) this._price = value;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
 
 // console.log(pers);
