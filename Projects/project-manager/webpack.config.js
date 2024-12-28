737af7067cba7b861793864fb69f0fdbd8853cbc
Julian <97186369+JulleK@users.noreply.github.com>
2024-12-28 15:18:17 +0100

    webpack production config

--- Projects/project-manager/webpack.config.js
+++ Projects/project-manager/webpack.config.js
@@ -2,19 +2,11 @@ const path = require("path");
 const CleanPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/app.ts",
-  devServer: {
-    static: [
-      {
-        directory: path.join(__dirname),
-      },
-    ],
-  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
   },
   module: {
     rules: [
