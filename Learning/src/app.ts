7eefaec346c01411c94d0cb74327455c9765408a
Julian <97186369+JulleK@users.noreply.github.com>
2024-12-28 17:50:02 +0100

    lodash with typescript

--- Learning/src/app.ts
+++ Learning/src/app.ts
@@ -1,1 +1,14 @@
-console.log("asd");
+import _ from "lodash";
+
+// ---- array ----
+// const arr = [1, 2, 3, "a", "b", "c", "hello", "world", true, false, "", 0];
+// const rand = Math.floor(Math.random() * 5) + 1;
+
+// const shuffled = _.shuffle(arr);
+// const chunked = _.chunk(shuffled, rand);
+
+// console.log(chunked);
+// console.log(_.last(chunked));
+
+// ---- date ----
+// console.log(_.now());
